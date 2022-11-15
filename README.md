88c105fc60e8572b72ed5c5156b210083a25c04f
Benjamin Xia <benjx32@gmail.com>
2022-11-15 12:25:23 -0800

    lab 8 100% speedrun (NEW WR)

--- README.md
+++ README.md
@@ -1,7 +1,14 @@
 # Lab 8 - Starter
 
-1. Where would you fit your automated tests in your Recipe project development pipeline? Select one of the following and explain why.  
+## Partners
+
+1. 笨虾 ben xia
+2. Nobody because I have no friends :(
+
+3. Where would you fit your automated tests in your Recipe project development pipeline? Select one of the following and explain why.  
    After code is pushed with a github action. You can't guarantee people will run the tests before pushing, may as well publicly humiliate them. 
-2. Would you use an end to end test to check if a function is returning the correct output? (yes/no)  
+4. Would you use an end to end test to check if a function is returning the correct output? (yes/no)  
    No
-3. 
+5. No, unit tests are just for checking function input/output. Messaging requires a lot of pieces
+   and an end to end test would probably be the best.
+6. Yes, just a simple function so you can just test input/output. return string.length <= max_msg_len;
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -3,3 +3,37 @@
 const functions = require('../code-to-unit-test/unit-test-me.js');
 
 // TODO - Part 2
+test('Phone number', () => {
+    expect(functions.isPhoneNumber("111-1111")).toBe(true);
+    expect(functions.isPhoneNumber("abcd")).toBe(false);
+    expect(functions.isPhoneNumber('(123)-123-1234')).toBe(true);
+    expect(functions.isPhoneNumber("1234-123-123tg1")).toBe(false);
+});
+
+test('Email', () => {
+    expect(functions.isEmail("waduhek@gmail.com")).toBe(true);
+    expect(functions.isEmail("af;lskdjf;laksdjf;laksdfj")).toBe(false);
+    expect(functions.isEmail("i_am_very_dumb@yahoo.com")).toBe(true);
+    expect(functions.isEmail("iforgotheat")).toBe(false);
+});
+
+test('strong password', () => {
+    expect(functions.isStrongPassword("password")).toBe(true); // ????????
+    expect(functions.isStrongPassword("123456")).toBe(false);
+    expect(functions.isStrongPassword("abc")).toBe(false);
+    expect(functions.isStrongPassword("iam_very")).toBe(true);
+})
+
+test('is date', () => {
+    expect(functions.isDate('1-1-1')).toBe(false);
+    expect(functions.isDate('1/1/2001')).toBe(true);
+    expect(functions.isDate('November 14, 2001')).toBe(false);
+    expect(functions.isDate('11/15/2022')).toBe(true);
+});
+
+test('is hex', () => {
+    expect(functions.isHexColor('FFFFFF')).toBe(true);
+    expect(functions.isHexColor("244,255,255")).toBe(false);
+    expect(functions.isHexColor("i am incredibly stupid")).toBe(false);
+    expect(functions.isHexColor("000000")).toBe(true);
+});
